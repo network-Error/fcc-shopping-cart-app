b6ac81c6f5ab370eb7cb58aa1c3dd5a8a1fb757d
network-Error <iamimew@gmail.com>
2024-04-09 22:30:14 +0300

    init: start task

---
+++ src/components/header/product-title/ProductTitle.jsx
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, removeFromCart } from '../../../store/slices/cart-slice';
+
+const ProductTitle = ({product}) => {
+  const dispatch = useDispatch();
+  const {cart} = useSelector(state => state);
+
+  function handleAddToCart() {
+    dispatch(addToCart(product))
+  }
+
+  function handleRemoveToCart() {
+    dispatch(removeFromCart(product.id));
+  }
+
+  return (
+    <div>
+      <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+        <div className="h-[180px]">
+          <img className="object-cover h-full w-full" src={product?.image} alt={product?.title} />
+        </div>
+        <div>
+          <h1 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">{product?.title}</h1>
+        </div>
+        <div className="flex items-center justify-center w-full mt-5">
+          <button onClick={cart.some(item => item.id === product.id) ? handleRemoveToCart : handleAddToCart} className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+            {
+              cart.some(item => item.id === product.id) ? 
+              'Remove from cart' :
+              'Add to cart'
+            }
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductTitle
